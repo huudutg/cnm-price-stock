6ebe5014233fb59423421b7747e8b40ba680d38b
doan199x <69084195+doan199x@users.noreply.github.com>
2021-07-15 20:38:48 +0700

    add function

---
+++ fe/src/functions/index.js
@@ -0,0 +1,29 @@
+
+import { AXIOS_INSTANCE } from './../config/Axios/index';
+export const callData = (stock, feature, model) => {
+    let featureParse = "";
+
+    featureParse += "close";
+    if (feature.includes("poc")) {
+        featureParse += "_poc";
+    }
+    if (feature.includes("rsi")) {
+        featureParse += "_rsi";
+    }
+    if (feature.includes("bb")) {
+        featureParse += "_bb";
+    }
+    return AXIOS_INSTANCE.get(`/api/predict?stock=${stock}&model=${model}_${featureParse}&start=2009-10-02`)
+    // return new Promise( (resolve, reject) => {
+    //     AXIOS_INSTANCE.get('/api/predict?stock=aapl&model=lstm_close_rsi&start=2020-01-01')
+    //     .then(res => resolve(() => {
+    //         res.data.csv.map(data => ({
+    //             ...data,
+    //             date: new Date(data.date)
+    //         }));
+    //         return res;
+    //     }))
+    //     .catch(reject)
+    // });
+}
+
