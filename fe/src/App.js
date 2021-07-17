4526289b2e2039cc24b46d6699d4ef657a1e15ac
doan199x <69084195+doan199x@users.noreply.github.com>
2021-07-17 15:49:43 +0700

    update app

---
+++ fe/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ChartComponent from './ChartComponent';
+import 'antd/dist/antd.css'; 
+
+function App() {
+  return (
+    <ChartComponent/>
+  );
+}
+
+export default App;
